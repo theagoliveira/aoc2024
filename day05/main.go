648c690c87719d88ede34aa096079496ecdf3787
Thiago Cavalcante <thiago.kun@gmail.com>
2024-12-07 16:19:21 -0300

    feat: use sort in day 05

--- day05/main.go
+++ day05/main.go
@@ -11,56 +11,33 @@ import (
 func main() {
 	rules, updateMaps, updateSlices := setup()
 
-	incorrectUpdateMaps := []map[string]int{}
-	incorrectUpdateSlices := [][]string{}
-
 	sum := 0
+	incorrectSum := 0
 	for index, updateMap := range updateMaps {
 		updateSlice := updateSlices[index]
 		valid := true
 
 		for _, rule := range rules {
 			if !checkRule(rule, updateMap) {
-				incorrectUpdateMaps = append(incorrectUpdateMaps, updateMap)
-				incorrectUpdateSlices = append(incorrectUpdateSlices, updateSlice)
+				slices.SortFunc(updateSlice, func(a, b int) int {
+					return compareRules(rules, a, b)
+				})
+				incorrectSum += getMiddleElement(updateSlice)
 				valid = false
 				break
 			}
 		}
 
 		if valid {
-			sum += getMiddleElementAsInt(updateSlice)
+			sum += getMiddleElement(updateSlice)
 		}
-	}
-
-	incorrectSum := 0
-	for index, updateMap := range incorrectUpdateMaps {
-		updateSlice := incorrectUpdateSlices[index]
-
-		for {
-			valid := true
-			for _, rule := range rules {
-				if !checkRule(rule, updateMap) {
-					updateSlice = putBefore(updateSlice, updateMap[rule[1]]-1, updateMap[rule[0]]-1)
-					updateMap = createIndexMap(updateSlice)
-					valid = false
-					break
-				}
-			}
-
-			if valid {
-				break
-			}
-		}
-
-		incorrectSum += getMiddleElementAsInt(updateSlice)
 	}
 
 	fmt.Println("sum:         ", sum)
 	fmt.Println("incorrectSum:", incorrectSum)
 }
 
-func setup() ([][]string, []map[string]int, [][]string) {
+func setup() ([][]int, []map[int]int, [][]int) {
 	input, _ := os.ReadFile("input.txt")
 	inputs := strings.Split(string(input), "\n\n")
 
@@ -69,15 +46,15 @@ func setup() ([][]string, []map[string]int, [][]string) {
 	ruleLines := strings.Split(ruleInput, "\n")
 	updateLines := strings.Split(updateInput, "\n")
 
-	rules := [][]string{}
+	rules := [][]int{}
 	for _, ruleLine := range ruleLines {
-		rules = append(rules, strings.Split(ruleLine, "|"))
+		rules = append(rules, convertToInt(strings.Split(ruleLine, "|")))
 	}
 
-	updateMaps := []map[string]int{}
-	updateSlices := [][]string{}
+	updateMaps := []map[int]int{}
+	updateSlices := [][]int{}
 	for _, updateLine := range updateLines {
-		updateSlice := strings.Split(updateLine, ",")
+		updateSlice := convertToInt(strings.Split(updateLine, ","))
 
 		updateMaps = append(updateMaps, createIndexMap(updateSlice))
 		updateSlices = append(updateSlices, updateSlice)
@@ -86,8 +63,17 @@ func setup() ([][]string, []map[string]int, [][]string) {
 	return rules, updateMaps, updateSlices
 }
 
-func createIndexMap(slice []string) map[string]int {
-	indexMap := map[string]int{}
+func convertToInt(array []string) []int {
+	nums := []int{}
+	for _, str := range array {
+		num, _ := strconv.Atoi(str)
+		nums = append(nums, num)
+	}
+	return nums
+}
+
+func createIndexMap(slice []int) map[int]int {
+	indexMap := map[int]int{}
 
 	for index, page := range slice {
 		indexMap[page] = index + 1
@@ -96,19 +82,34 @@ func createIndexMap(slice []string) map[string]int {
 	return indexMap
 }
 
-func checkRule(rule []string, updateMap map[string]int) bool {
+func checkRule(rule []int, updateMap map[int]int) bool {
 	return updateMap[rule[0]] == 0 ||
 		updateMap[rule[1]] == 0 ||
 		updateMap[rule[0]] < updateMap[rule[1]]
 }
 
-func putBefore(slice []string, x, y int) []string {
-	valueY := slice[y]
-	sliceWithoutY := append(slice[:y], slice[y+1:]...)
-	return slices.Insert(sliceWithoutY, x, valueY)
+func getMiddleElement(slice []int) int {
+	return slice[(len(slice)-1)/2]
 }
 
-func getMiddleElementAsInt(slice []string) int {
-	result, _ := strconv.Atoi(slice[(len(slice)-1)/2])
-	return result
+func compareRules(rules [][]int, a, b int) int {
+	for _, rule := range rules {
+		if rule[0] == a && rule[1] == b {
+			return -1
+		} else if rule[0] == b && rule[1] == a {
+			return 1
+		}
+	}
+
+	return compare(a, b)
+}
+
+func compare(a, b int) int {
+	if a == b {
+		return 0
+	}
+	if a < b {
+		return -1
+	}
+	return 1
 }

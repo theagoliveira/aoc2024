34e92ea50a3c9e408b53eee35fe910bea56b4a90
Thiago Cavalcante <thiago.kun@gmail.com>
2024-12-07 18:16:36 -0300

    feat: use dfs in day 6

--- day06/main.go
+++ day06/main.go
@@ -4,46 +4,30 @@ import (
 	"fmt"
 	"math"
 	"os"
+	"reflect"
+	"slices"
 	"strings"
 )
 
 const (
-	guard    = "^"
-	wall     = "#"
-	visited  = "X"
-	obstacle = "O"
+	guard       = "^"
+	wall        = "#"
+	visited     = "X"
+	visitedLoop = "Y"
+	obstacle    = "O"
 )
 
 func main() {
-	guardMap, gx, gy := setup()
+	guardMap, gx, gy := setup("test.txt")
 
-	sum := 0
-	angle := math.Pi
-	dx, dy := calcDirection(angle)
-	for {
-		if guardMap[gx][gy] != visited {
-			sum++
-			guardMap[gx][gy] = visited
-		}
+	sum, loops := dfs(guardMap, gx, gy, math.Pi)
 
-		if !isValid(guardMap, gx+dx, gy+dy) {
-			break
-		}
-
-		if guardMap[gx+dx][gy+dy] == wall {
-			angle = nextAngle(angle)
-			dx, dy = calcDirection(angle)
-		}
-
-		gx += dx
-		gy += dy
-	}
-
-	fmt.Println("sum:", sum)
+	fmt.Println("sum:  ", sum)
+	fmt.Println("loops:", len(loops))
 }
 
-func setup() ([][]string, int, int) {
-	input, _ := os.ReadFile("input.txt")
+func setup(file string) ([][]string, int, int) {
+	input, _ := os.ReadFile(file)
 	lines := strings.Split(string(input), "\n")
 
 	guardMap := [][]string{}
@@ -70,4 +54,111 @@ func nextAngle(angle float64) float64 {
 
 func calcDirection(angle float64) (int, int) {
 	return int(math.Cos(angle)), int(math.Sin(angle))
+}
+
+func dfs(array [][]string, x, y int, angle float64) (int, []map[string]struct{}) {
+	count := 0
+	loops := []map[string]struct{}{}
+	dx, dy := calcDirection(angle)
+
+	if array[x][y] != visited {
+		array[x][y] = visited
+		count = 1
+	}
+
+	if !isValid(array, x+dx, y+dy) {
+		return count, loops
+	}
+
+	if array[x+dx][y+dy] == wall {
+		angle = nextAngle(angle)
+		dx, dy = calcDirection(angle)
+	}
+
+	arrayCopy := copy(array)
+	arrayCopy[x+dx][y+dy] = obstacle
+	parents := createEmptyArrayFrom(arrayCopy)
+	loop := dfsLoop(arrayCopy, parents, x, y, angle)
+
+	prevCount, prevLoops := dfs(array, x+dx, y+dy, angle)
+
+	loops = prevLoops
+	if len(loop) != 0 && !arrayContains(loops, loop) {
+		loops = append(loops, loop)
+	}
+
+	return prevCount + count, loops
+}
+
+func dfsLoop(array [][]string, parent [][][]int, x, y int, angle float64) map[string]struct{} {
+	dx, dy := calcDirection(angle)
+
+	if array[x][y] != visitedLoop {
+		array[x][y] = visitedLoop
+	}
+
+	if !isValid(array, x+dx, y+dy) {
+		return map[string]struct{}{}
+	}
+
+	if array[x+dx][y+dy] == wall || array[x+dx][y+dy] == obstacle {
+		angle = nextAngle(angle)
+		dx, dy = calcDirection(angle)
+	}
+
+	if array[x+dx][y+dy] == visitedLoop && slices.Equal(parent[x+dx][y+dy], []int{x, y}) {
+		result := map[string]struct{}{}
+		for i, row := range parent {
+			for j, col := range row {
+				if len(col) > 0 {
+					result[fmt.Sprintf("[%d,%d,%d,%d]", i, j, col[0], col[1])] = struct{}{}
+				}
+			}
+		}
+		return result
+	}
+
+	parent[x+dx][y+dy] = []int{x, y}
+	return dfsLoop(array, parent, x+dx, y+dy, angle)
+}
+
+func printArray(array [][]string) {
+	for _, line := range array {
+		for _, elem := range line {
+			fmt.Print(elem)
+		}
+		fmt.Println()
+	}
+	fmt.Println()
+}
+
+func copy(array [][]string) [][]string {
+	copy := [][]string{}
+	for _, elem := range array {
+		copy = append(copy, append([]string{}, elem...))
+	}
+
+	return copy
+}
+
+func createEmptyArrayFrom(array [][]string) [][][]int {
+	emptyArray := [][][]int{}
+	for i := 0; i < len(array); i++ {
+		emptyArray = append(emptyArray, [][]int{})
+		for j := 0; j < len(array[i]); j++ {
+			emptyArray[i] = append(emptyArray[i], []int{})
+		}
+	}
+
+	return emptyArray
+}
+
+func arrayContains(array []map[string]struct{}, new map[string]struct{}) bool {
+	for i := 0; i < len(array); i++ {
+		if reflect.DeepEqual(array[i], new) {
+			return true
+		}
+	}
+
+	return false
 }
